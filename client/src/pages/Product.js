0018f209c274454b4b748c2d9f964e8f3c7042bd
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-06-05 18:50:53 +0700

    update

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -4,6 +4,7 @@ import NavbarUser from '../components/User/NavbarUser';
 import cssModule from '../css/Product.module.css';
 import convertRupiah from "rupiah-format";
 import { Spinner } from 'react-bootstrap';
+import { useQuery } from 'react-query'
 
 import { API } from '../config/api';
 
@@ -13,7 +14,22 @@ export default function Product() {
 
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
+    const [category, setCategory] = useState(posts);
+    console.log(category);
     const [searchTitle, setSearchTitle] = useState("");
+
+    const filterItem = ((categItem) => {
+        const updateditem = posts.filter((curElem) => {
+            return curElem.categories[0].nameCategory === categItem;
+        });
+        setCategory(updateditem);
+    });
+
+    const allItem = () => {
+        setCategory(posts)
+    }
+    // console.log(allItem);
+
 
     const config = {
         method: "GET",
@@ -29,7 +45,7 @@ export default function Product() {
             setPosts(response.data.data);
             setTimeout(() => {
                 setLoading(false)
-            }, 600)
+            })
         }
         loadPosts();
     }, []);
@@ -42,15 +58,31 @@ export default function Product() {
                     placeholder='Search Product'
                     onChange={(e) => setSearchTitle(e.target.value)} />
                 <h3 className='mt-3'>DumbMerch</h3>
+                <div>
+                    <button className='btn btn-warning' onClick={() => filterItem('Laptop')}>laptop</button>
+                    <button className='btn btn-warning' onClick={() => filterItem('Handphone')}>Mouse</button>
+                    <button className='btn btn-warning' onClick={() => setCategory(posts)}>All</button>
+                </div>
             </div>
+            {loading ? <div className='container'>waiting</div> : <></>}
             <div className="container d-flex flex-wrap mt-3">
-                {loading ? (<>
-                    <Spinner animation="grow" variant="danger" />
-                    <Spinner animation="grow" variant="warning" />
-                    <Spinner animation="grow" variant="primary" />
+                {category?.length === 0 ? (<>
+                    {posts.map((item, index) => (
+                        <div>
+                            <div item={item} key={index}></div>
+                            <div className={cssModule.product1}>
+                                <img src={item.image} width={'100%'} height={'200px'} className='img-fluid mb-2' alt={item.name} />
+                                <div>
+                                    <Link to={`/ detail / ${item.id}`}><p className='ps-2'><span>{item.name}</span ></p></Link>
+                                    <p className='ps-2'>{convertRupiah.convert(item.price)}</p>
+                                    <p className='ps-2'>Stok: {item.qty}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </>
                 ) : (
-                    posts
+                    posts && category
                         .filter((value) => {
                             if (searchTitle === '') {
                                 return value;
@@ -66,7 +98,7 @@ export default function Product() {
                                     <div className={cssModule.product1}>
                                         <img src={item.image} width={'100%'} height={'200px'} className='img-fluid mb-2' alt={item.name} />
                                         <div>
-                                            <Link to={`/detail/${item.id}`}><p className='ps-2'><span>{item.name}</span ></p></Link>
+                                            <Link to={`/ detail / ${item.id}`}><p className='ps-2'><span>{item.name}</span ></p></Link>
                                             <p className='ps-2'>{convertRupiah.convert(item.price)}</p>
                                             <p className='ps-2'>Stok: {item.qty}</p>
                                         </div>
